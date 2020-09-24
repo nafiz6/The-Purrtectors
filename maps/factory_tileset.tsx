d6ed38a40f80a6d677627c1fc7892bb68666d412
Abrar <abrar9597@gmail.com>
2020-09-24 23:43:44 +0600

    factory_tileset not found fix

---
+++ maps/factory_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="factory_tileset" tilewidth="16" tileheight="16" tilecount="1664" columns="52">
+ <image source="finals/factory_tileset.png" width="832" height="512"/>
+</tileset>
